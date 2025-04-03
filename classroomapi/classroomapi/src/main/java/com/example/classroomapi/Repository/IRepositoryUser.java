d26f25e58b5b37cc128fc11da84ab31e51a3ea25
IsabelaChaverra <canochaverraisabela@gmail.com>
2025-04-03 10:22:54 -0500

    Upload

--- classroomapi/classroomapi/src/main/java/com/example/classroomapi/Repository/IRepositoryUser.java
+++ classroomapi/classroomapi/src/main/java/com/example/classroomapi/Repository/IRepositoryUser.java
@@ -4,6 +4,6 @@ import com.example.classroomapi.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
+@Repository
 public interface IRepositoryUser extends JpaRepository<User, Integer> {
 }
